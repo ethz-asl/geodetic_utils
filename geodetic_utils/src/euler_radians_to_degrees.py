d18c910e483cbb6617105a4227c87c8a07c76e1e
asl <fmina@ethz.ch>
2015-10-23 17:23:30 +0200

    Added radian to degree conversion for fcu IMU topic

--- geodetic_utils/src/euler_radians_to_degrees.py
+++ geodetic_utils/src/euler_radians_to_degrees.py
@@ -6,15 +6,25 @@ import rospy
 from geometry_msgs.msg import Vector3
 import numpy
 
-def callback(message):
+def callback_compass(message):
   degrees = Vector3()
   degrees.x = numpy.rad2deg(message.x)
   degrees.y = numpy.rad2deg(message.y)
   degrees.z = numpy.rad2deg(message.z)
-  degrees_pub.publish(degrees)
+  degrees_compass_pub.publish(degrees)
 
-radians_sub = rospy.Subscriber('/orientation_euler', Vector3, callback)
-degrees_pub = rospy.Publisher('/orientation_euler_degrees', Vector3, queue_size=10)
+def callback_fcu(message):
+  degrees = Vector3()
+  degrees.x = numpy.rad2deg(message.x)
+  degrees.y = numpy.rad2deg(message.y)
+  degrees.z = numpy.rad2deg(message.z)
+  degrees_fcu_pub.publish(degrees)
+
+radians_compass_sub = rospy.Subscriber('/orientation_euler_compass', Vector3, callback_compass)
+radians_fcu_sub = rospy.Subscriber('/orientation_euler_fcu', Vector3, callback_fcu)
+
+degrees_compass_pub = rospy.Publisher('/orientation_euler_compass_degrees', Vector3, queue_size=10)
+degrees_fcu_pub = rospy.Publisher('/orientation_euler_fcu_degrees', Vector3, queue_size=10)
 
 if __name__ == '__main__':
   rospy.init_node('euler_degrees_to_radians', anonymous=True)
