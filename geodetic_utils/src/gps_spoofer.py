768188400ac1f601ffe546fdb370b8ebcb0c9974
Enric Galceran <enricg@ethz.ch>
2015-11-03 16:10:53 +0100

    Adding GPS spoofer.

---
+++ geodetic_utils/src/gps_spoofer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import NavSatFix
+
+if __name__ == '__main__':
+
+    try:    
+        rospy.init_node('gps_spoofer', anonymous=True)
+        pub = rospy.Publisher('spoofed_gps', NavSatFix, queue_size=1)
+        rate = rospy.Rate(5) # 5hz
+
+        fix = NavSatFix()
+        fix.header.stamp = rospy.Time.now()
+        fix.header.frame_id = 'fcu'
+        fix.status.status = 1
+        fix.status.service = 1
+        fix.latitude = 44.0
+        fix.longitude = 44.0
+        fix.altitude = 0.0
+        # TODO: fill covariance        
+        
+        while not rospy.is_shutdown():
+            pub.publish(fix)
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
