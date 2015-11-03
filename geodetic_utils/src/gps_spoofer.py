9680c54dac81a7d91785d1fd9a88aae11905e159
Enric Galceran <enricg@ethz.ch>
2015-11-03 16:51:44 +0100

    Updating timestamp in GPS spoofer.

--- geodetic_utils/src/gps_spoofer.py
+++ geodetic_utils/src/gps_spoofer.py
@@ -3,25 +3,30 @@
 import rospy
 from sensor_msgs.msg import NavSatFix
 
+class GpsSpoofer:
+    def __init__(self):
+        self.fix = NavSatFix()
+        #self.fix.header.stamp = ...
+        self.fix.header.frame_id = 'fcu'
+        self.fix.status.status = 1
+        self.fix.status.service = 1
+        self.fix.latitude = 44.0
+        self.fix.longitude = 44.0
+        self.fix.altitude = 0.0
+        # TODO: fill covariance
+        
+        self.pub = rospy.Publisher('spoofed_gps', NavSatFix, queue_size=1)
+        self.sub = rospy.Subscriber("fcu/gps", NavSatFix, self.callback)
+    
+    def callback(self, data):
+        self.fix.header.stamp = data.header.stamp
+        self.pub.publish(fix)        
+
 if __name__ == '__main__':
 
     try:    
         rospy.init_node('gps_spoofer', anonymous=True)
-        pub = rospy.Publisher('spoofed_gps', NavSatFix, queue_size=1)
-        rate = rospy.Rate(5) # 5hz
-
-        fix = NavSatFix()
-        fix.header.stamp = rospy.Time.now()
-        fix.header.frame_id = 'fcu'
-        fix.status.status = 1
-        fix.status.service = 1
-        fix.latitude = 44.0
-        fix.longitude = 44.0
-        fix.altitude = 0.0
-        # TODO: fill covariance        
-        
-        while not rospy.is_shutdown():
-            pub.publish(fix)
-            rate.sleep()
+        gs = GpsSpoofer()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
