ef70b50e5c3d7ea49217ed0cd083536acb84dea5
Christian Lanegger <clanegge@ethz.ch>
2020-07-17 19:37:54 +0200

    Added wrappers for rest of member functions
    Needed some special hackery for overloaded functions and functions with pointers as arguments.

--- geotf_python/src/export_geodetic_converter.cc
+++ geotf_python/src/export_geodetic_converter.cc
@@ -3,24 +3,66 @@
 
 using gc = geotf::GeodeticConverter;
 
-// No idea if this works.. am tired..
+// Thin wrapper for exposing default argument
 void initFromRosParamNoArgs(gc &geodetic_converter) {
   geodetic_converter.initFromRosParam();
 }
 
-void initFromRosParamStr(gc &geodetic_converter, const std::string& prefix) {
+void initFromRosParamStr(gc &geodetic_converter, const std::string &prefix) {
   geodetic_converter.initFromRosParam(prefix);
+}
+
+Eigen::Affine3d convertFromTfDefault(gc &geodetic_converter,
+                                     const std::string &geo_input_frame,
+                                     const Eigen::Affine3d &input,
+                                     const std::string &tf_output_frame) {
+  Eigen::Affine3d output;
+  geodetic_converter.convertToTf(geo_input_frame, input, tf_output_frame,
+                                 &output);
+  return output;
+}
+
+Eigen::Affine3d convertFromTfTime(gc &geodetic_converter,
+                                  const std::string &geo_input_frame,
+                                  const Eigen::Affine3d &input,
+                                  const std::string &tf_output_frame,
+                                  const ros::Time &time) {
+  Eigen::Affine3d output;
+  geodetic_converter.convertToTf(geo_input_frame, input, tf_output_frame,
+                                 &output, time);
+  return output;
+}
+
+// Wrapper for overloaded functions (workaround for output pointer)
+Eigen::Vector3d convertVec(gc &geodetic_converter,
+                           const std::string &input_frame,
+                           const Eigen::Vector3d &input,
+                           const std::string &output_frame) {
+  Eigen::Vector3d output;
+  geodetic_converter.convert(input_frame, input, output_frame, &output);
+  return output;
+}
+
+Eigen::Affine3d convertAff(gc &geodetic_converter,
+                           const std::string &input_frame,
+                           const Eigen::Affine3d &input,
+                           const std::string &output_frame) {
+  Eigen::Affine3d output;
+  geodetic_converter.convert(input_frame, input, output_frame, &output);
+  return output;
 }
 
 void exportGeodeticConverter() {
   using namespace boost::python;
 
-  bool (gc::*convert_aff)(const std::string &, const Eigen::Affine3d &,
-                          const std::string &, Eigen::Affine3d *) =
-      &gc::convert;
-  bool (gc::*convert_vec)(const std::string &, const Eigen::Vector3d &,
-                          const std::string &, Eigen::Vector3d *) =
-      &gc::convert;
+  // Create objects for overloaded functions
+  bool (gc::*publishAsTfVec)(const std::string &geo_input_frame,
+                             const Eigen::Vector3d &input,
+                             const std::string &frame_name) = &gc::publishAsTf;
+
+  bool (gc::*publishAsTfAff)(const std::string &geo_input_frame,
+                             const Eigen::Affine3d &input,
+                             const std::string &frame_name) = &gc::publishAsTf;
 
   class_<gc, boost::shared_ptr<gc>>("GeodeticConverter", init<>())
       .def("initFromRosParam", initFromRosParamNoArgs)
@@ -33,7 +75,12 @@ void exportGeodeticConverter() {
       .def("removeFrame", &gc::removeFrame)
       .def("hasFrame", &gc::hasFrame)
       .def("canConvert", &gc::canConvert)
-      .def("convert", convert_aff)
-      .def("convert", convert_vec)
-      .def("convertToTf", &gc::convertToTf);
+      .def("convert", convertAff)
+      .def("convert", convertVec)
+      .def("convertToTf", &gc::convertToTf)
+      .def("convertFromTf", convertFromTfDefault)
+      .def("convertFromTf", convertFromTfTime)
+      .def("publishAsTf", publishAsTfAff)
+      .def("publishAsTf", publishAsTfVec)
+      .def("writeDebugInfo", &gc::writeDebugInfo);
 }
