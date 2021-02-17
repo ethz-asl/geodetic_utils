a7a24563928848b0c431abac9a07d58f98968175
michaelpantic <michael.pantic@gmail.com>
2021-02-17 22:02:53 +0100

    Noise generator for GPS simulator

---
+++ gps_sim/scripts/GpsNoiser.py
@@ -0,0 +1,63 @@
+import colorednoise as cn
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class GpsNoiser:
+
+    # multiplier for each of the noise types, each of the axes
+    def __init__(self, white=np.array([0.1, 0.1, 0.1]),
+                        pink=np.array([0.05, 0.05, 0.6]),
+                        brown=np.array([0.5, 0.5, 0.1])):
+        self._white = white
+        self._pink = pink
+        self._brown = brown
+        self._sample_buffer_length = 500  # [s]
+        self._sampling_freq = 10  # [hz]
+        # order: white, pink, brown
+        self._sample_buffer = np.zeros([self._sample_buffer_length * self._sampling_freq, 9])
+        self._sample_buffer_pos = 0
+
+    def sampleNoise(self):
+        # make some noise!
+        n = self._sample_buffer_length * self._sampling_freq
+        for i in range(0, 3):
+            self._sample_buffer[:, i] = cn.powerlaw_psd_gaussian(0, n)  # white
+            self._sample_buffer[:, i + 3] = cn.powerlaw_psd_gaussian(1, n)  # pink
+            self._sample_buffer[:, i + 6] = cn.powerlaw_psd_gaussian(2, n)  # brown
+
+    def getNoise(self, pos_enu):
+        i = self._sample_buffer_pos
+
+        if self._sample_buffer_pos >= self._sample_buffer_length * self._sampling_freq:
+            print("Run out of sampling buffer")
+        else:
+            self._sample_buffer_pos += 1
+
+        return pos_enu + np.multiply(self._white, self._sample_buffer[i, 0:3]) + \
+               np.multiply(self._pink, self._sample_buffer[i, 3:6]) + \
+               np.multiply(self._brown, self._sample_buffer[i, 6:9])
+
+
+if __name__ == "__main__":
+    a = GpsNoiser()
+    true_pos = np.array([0, 0, 0])
+    a.sampleNoise()
+    disturbed_pos = np.zeros([1000,3])
+
+    for i in range(0,1000):
+        disturbed_pos[i,:] = a.getNoise(true_pos)
+
+
+    fig, ax = plt.subplots()
+    ax.scatter(disturbed_pos[:, 0], disturbed_pos[:, 1],c = np.arange(1000), marker='x')
+    #ax.plot(disturbed_pos[0:60,0],'r')
+    #ax.plot(disturbed_pos[0:60, 1], 'g')
+    #ax.plot(disturbed_pos[0:60, 2], 'b')
+    ax.set_xlabel("ENU_X noise")
+    ax.set_ylabel("ENU_Y noise")
+    ax.set_title("combined example")
+    ax.grid()
+    plt.show()
